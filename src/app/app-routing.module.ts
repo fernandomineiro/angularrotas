919a8f892b84867d8c182903e01d08dff3eeee04
fernando <fernandofitilan@hotmail.com>
2019-12-09 17:14:54 -0200

    upando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { PostsComponent } from './posts/posts.component';
+import { CreateComponent } from './posts/create/create.component';
+import { DetailComponent} from './posts/detail/detail.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {
+      path: '',
+      component: HomeComponent
+  },
+  {
+      path: 'posts',
+      children: [
+        {
+          path: '',
+          component: PostsComponent
+        },
+        {
+            path: 'criar',
+            component: CreateComponent
+        },
+        {
+            path: 'detalhes',
+            component: DetailComponent
+        }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PostsComponent } from './posts/posts.component';
+import { CreateComponent } from './posts/create/create.component';
+import { DetailComponent } from './posts/detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PostsComponent,
+    CreateComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
